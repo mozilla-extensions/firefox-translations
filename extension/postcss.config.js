c5143a3c5bc602c19161b583a5fb25eedbfcb576
Motin <motin@motin.eu>
2020-11-02 16:32:30 +0200

    Postcss configuration and css changes to accept the main-interface scss

---
+++ extension/postcss.config.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+
+const options = {
+  parser: 'postcss-scss',
+  plugins: [
+    require("tailwindcss")("./tailwind.config.js"),
+    require('postcss-nested'),
+    require("postcss-preset-env"),
+  ],
+};
+
+module.exports = options;
