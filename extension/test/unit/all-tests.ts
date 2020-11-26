8b89df9f006b12e21e6a43ca6387a1b7b3a6c697
Motin <motin@motin.eu>
2020-11-26 15:28:35 +0200

    Move (node-backed) unit tests to a specific subfolder + minor cleanup

--- extension/test/unit/all-tests.ts
+++ extension/test/unit/all-tests.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-const context = require.context("../ts", true, /.spec.ts$/);
+const context = require.context("../../ts", true, /.spec.ts$/);
 // @ts-ignore
 context.keys().forEach(context);
 module.exports = context;
