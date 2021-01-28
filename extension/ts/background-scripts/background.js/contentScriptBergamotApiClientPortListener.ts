4fb40964a1d2af694bca454e41694b37bb60ca61
Motin <motin@motin.eu>
2021-01-28 21:01:30 +0200

    Proof of concept implementation of using bergamot translator wasm apis in a worker thread as the translation backend

--- extension/ts/background-scripts/background.js/contentScriptBergamotApiClientPortListener.ts
+++ extension/ts/background-scripts/background.js/contentScriptBergamotApiClientPortListener.ts
@@ -4,10 +4,16 @@
 
 import {
   BergamotApiClient,
+  BergamotRestApiParagraph,
+  BergamotRestApiSentence,
   BergamotRestApiTranslateRequestResult,
 } from "./lib/BergamotApiClient";
 import { Runtime } from "webextension-polyfill-ts";
 import Port = Runtime.Port;
+import {
+  BergamotTranslatorAPI,
+  TranslationResults,
+} from "./lib/BergamotTranslatorAPI";
 const bergamotApiClient = new BergamotApiClient();
 
 export const contentScriptBergamotApiClientPortListener = (port: Port) => {
@@ -20,9 +26,29 @@ export const contentScriptBergamotApiClientPortListener = (port: Port) => {
   }) {
     // console.debug("Message from content-script-bergamot-api-client:", {m});
     const { texts, requestId } = m;
+    /*
     const results: BergamotRestApiTranslateRequestResult = await bergamotApiClient.sendTranslationRequest(
       texts,
     );
+     */
+    const translatorApiResults: TranslationResults = await BergamotTranslatorAPI.translate(
+      texts,
+      "_",
+      "_",
+    );
+    const paragraphs: BergamotRestApiParagraph[] = translatorApiResults.translatedTexts.map(
+      text => {
+        const sentenceList: BergamotRestApiSentence[] = [
+          { nBest: [{ translation: text }] },
+        ];
+        return {
+          0: sentenceList,
+        };
+      },
+    );
+    const results: BergamotRestApiTranslateRequestResult = {
+      text: paragraphs,
+    };
     // console.log({ results });
     try {
       port.postMessage({
