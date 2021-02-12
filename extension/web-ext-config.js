c6c4e723639bd6554503e6f1c1d0acb4bb6e5920
Motin <motin@motin.eu>
2021-02-12 13:39:31 +0200

    Add larger wikipedia fixture

--- extension/web-ext-config.js
+++ extension/web-ext-config.js
@@ -2,6 +2,7 @@
 const path = require("path");
 
 const examplePagesToTranslate = [
+  "http://0.0.0.0:4000/es.wikipedia.org-2020-11-21/original.html",
   "http://0.0.0.0:4000/es.wikipedia.org-2021-01-20-welcome-box/original.html",
   "https://es.wikipedia.org",
   "https://www.mozilla.org/",
