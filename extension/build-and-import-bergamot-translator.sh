15e0aad389e76fa863748adbf9b029b3e0a2fd49
Motin <motin@motin.eu>
2021-02-15 11:38:53 +0200

    Use docker-based reproducible bergamot-translator builds

--- extension/build-and-import-bergamot-translator.sh
+++ extension/build-and-import-bergamot-translator.sh
@@ -1,11 +1,9 @@
 set -e
 set -x
 
-cd ../bergamot-translator
-mkdir -p build-wasm
-cd build-wasm
-emcmake cmake -DCOMPILE_WASM=on ../
-emmake make -j
+cd ../bergamot-translator/docker
+make
+cd ../build-wasm-docker
 cat wasm/bergamot-translator-worker.js ../../extension/src/wasm/bergamot-translator-worker.appendix.js > ../../extension/src/wasm/bergamot-translator-worker.js
 cp wasm/bergamot-translator-worker.wasm ../../extension/src/wasm/bergamot-translator-worker.wasm
 cp wasm/bergamot-translator-worker.data ../../extension/src/wasm/bergamot-translator-worker.data
