1e437ba702db36168eb7e8f14171e116bcd458c0
Motin <motin@motin.eu>
2021-02-18 12:21:36 +0200

    Not installing and activated emsdk if the EMSDK env var is already set

--- build-bergamot-translator.sh
+++ build-bergamot-translator.sh
@@ -12,28 +12,30 @@ cd "$(dirname $0)"
 # with slight adjustments to be able to run the build script multiple times without having to clone all dependencies
 # as per "As long as you don't update any submodule, just follow steps in `4.ii` to recompile."
 
-# 1. Download and Install Emscripten using following instructions
-EMSDK_UPDATE_REQUIRED=0
-if [ ! -d "emsdk" ]; then
-  git clone https://github.com/emscripten-core/emsdk.git
-  EMSDK_UPDATE_REQUIRED=1
-else
-  cd emsdk
-  git fetch
-  # Only pull if necessary
-  if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
-    git pull --ff-only
+# 1. Download and Install Emscripten using following instructions (unless the EMSDK env var is already set)
+if [ "$EMSDK" == "" ]; then
+  EMSDK_UPDATE_REQUIRED=0
+  if [ ! -d "emsdk" ]; then
+    git clone https://github.com/emscripten-core/emsdk.git
     EMSDK_UPDATE_REQUIRED=1
+  else
+    cd emsdk
+    git fetch
+    # Only pull if necessary
+    if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
+      git pull --ff-only
+      EMSDK_UPDATE_REQUIRED=1
+    fi
+    cd -
   fi
-  cd -
+  if [ "$EMSDK_UPDATE_REQUIRED" == "1" ]; then
+    cd emsdk
+    ./emsdk install latest
+    ./emsdk activate latest
+    cd -
+  fi
+  source ./emsdk/emsdk_env.sh
 fi
-if [ "$EMSDK_UPDATE_REQUIRED" == "1" ]; then
-  cd emsdk
-  ./emsdk install latest
-  ./emsdk activate latest
-  cd -
-fi
-source ./emsdk/emsdk_env.sh
 
 # 2. Clone the repository and checkout the appropriate branch using these instructions:
 git submodule update --init --recursive
