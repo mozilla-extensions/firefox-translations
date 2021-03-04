be349e7e78362d6ffc62780b62fa416ffe061bbe
evgeny pavlov <epavlov@mozilla.com>
2021-03-03 16:05:06 -0800

    Use test metrics

--- extension/ts/background-scripts/background.js/telemetry/Telemetry.spec.ts
+++ extension/ts/background-scripts/background.js/telemetry/Telemetry.spec.ts
@@ -4,38 +4,29 @@
 
 import { assert } from "chai";
 import { Telemetry } from "./Telemetry";
-import { closed, translate } from "./generated/infobar";
-import { fromLang, toLang } from "./generated/metadata";
-import { translationTime } from "./generated/performance";
+import { counterTest, eventTest, stringTest } from "./generated/test";
 
 const testSuite = "Telemetry";
 describe(testSuite, function() {
-  const testName = "telemetry test:test_metrics_collected";
+  const testName = "telemetry test:metrics_collected";
   it(testName, async function() {
     console.info(`${testSuite}: ${testName}`);
-    const langFrom = "es";
-    const langTo = "en";
-    const expectedTime = "125";
+    const testStrVal = "test";
     const pingName = "custom";
     const telemetry = new Telemetry();
-    // todo: figure out how to mock submitting
-    telemetry.setUploadEnabled(true);
 
-    telemetry.record(() => fromLang.set(langFrom), "testFromLang");
-    telemetry.record(() => toLang.set(langTo), "testLantTo");
-    telemetry.record(() => translate.record(), "testTranslate");
-    telemetry.record(() => closed.record(), "testClosed");
-    telemetry.record(() => translationTime.set(expectedTime), "testTime");
+    telemetry.record(() => eventTest.record(), "testEvent");
+    telemetry.record(() => counterTest.add(), "testCounter");
+    telemetry.record(() => stringTest.set(testStrVal), "testString");
 
-    assert.equal(1, (await translate.testGetValue(pingName)).length);
-    assert.equal(1, (await closed.testGetValue(pingName)).length);
-    assert.equal(langFrom, await fromLang.testGetValue(pingName));
-    assert.equal(langTo, await toLang.testGetValue(pingName));
-    assert.equal(expectedTime, await translationTime.testGetValue(pingName));
+    assert.equal(1, (await eventTest.testGetValue(pingName)).length);
+    assert.equal(1, await counterTest.testGetValue(pingName));
+    assert.equal(testStrVal, await stringTest.testGetValue(pingName));
 
     telemetry.submit();
-    assert.isUndefined(await translate.testGetValue(pingName));
-    assert.isUndefined(await fromLang.testGetValue(pingName));
+    assert.isUndefined(await eventTest.testGetValue(pingName));
+    assert.isUndefined(await counterTest.testGetValue(pingName));
+    assert.isUndefined(await stringTest.testGetValue(pingName));
 
     console.log("test passed");
   });
