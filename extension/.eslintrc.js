b8c0591692305539149d3d3c8a6589abb6b8b128
Motin <motin@motin.eu>
2021-03-05 08:11:19 +0200

    Hardened eslint config + removed tslint in favor of applying eslint also on ts files

--- extension/.eslintrc.js
+++ extension/.eslintrc.js
@@ -1,7 +1,5 @@
 /* eslint-env node */
 
-"use strict";
-
 // All Mozilla specific rules and environments at:
 // http://firefox-source-docs.mozilla.org/tools/lint/linters/eslint-plugin-mozilla.html
 
@@ -13,6 +11,7 @@ module.exports = {
     "eslint:recommended",
     // list of rules at: https://dxr.mozilla.org/mozilla-central/source/tools/lint/eslint/eslint-plugin-mozilla/lib/configs/recommended.js
     "plugin:mozilla/recommended",
+    "plugin:prettier/recommended",
   ],
   overrides: [
     {
@@ -20,6 +19,36 @@ module.exports = {
       env: {
         browser: true,
         webextensions: true,
+      },
+    },
+    {
+      files: ["ts/**/*.ts", "ts/**/*.tsx"],
+      extends: ["plugin:mocha/recommended"],
+      // the dotenv-webpack plugin exposes process.env.* based on the contents in .env.development or .env.production
+      globals: { process: "readonly" },
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      plugins: ["mocha"],
+      env: {
+        browser: true,
+        mocha: true,
+        webextensions: true,
+      },
+    },
+    {
+      files: "test/functional/**/*.ts",
+      extends: ["plugin:mocha/recommended"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./test/functional/tsconfig.json",
+      },
+      plugins: ["mocha"],
+      env: {
+        browser: true,
+        mocha: true,
+        node: true,
       },
     },
   ],
@@ -34,12 +63,11 @@ module.exports = {
   plugins: ["json", "mozilla"],
   root: true,
   rules: {
-    "babel/new-cap": "off",
-    "mozilla/no-aArgs": "warn",
-    "mozilla/balanced-listeners": "off",
+    "mozilla/no-aArgs": "error",
+    "mozilla/balanced-listeners": "error",
     "comma-dangle": ["error", "always-multiline"],
     eqeqeq: "error",
-    indent: ["warn", 2, { SwitchCase: 1 }],
+    indent: "off",
     "max-len": [
       "warn",
       {
@@ -55,7 +83,7 @@ module.exports = {
     "no-shadow": "error",
     "no-unused-vars": "error",
     "no-var": "error",
-    "prefer-const": "warn",
+    "prefer-const": "error",
     "prefer-spread": "error",
     semi: ["error", "always"],
     "valid-jsdoc": "warn",
