2efaea845b7a4ea8062ef0678e77946baee14496
Motin <motin@motin.eu>
2021-03-29 11:19:23 +0300

    Use Glean.testResetGlean in in-browser telemetry tests

--- src/core/ts/background-scripts/background.js/telemetry/Telemetry.spec.ts
+++ src/core/ts/background-scripts/background.js/telemetry/Telemetry.spec.ts
@@ -5,6 +5,8 @@
 import { assert } from "chai";
 import { telemetry } from "./Telemetry";
 import { counterTest, eventTest, stringTest } from "./generated/test";
+import Glean from "@mozilla/glean/webext";
+import { config } from "../../../config";
 
 const testStrVal = "test";
 const pingName = "custom";
@@ -13,8 +15,6 @@ const submitAndAssert = async () => {
   const eventTestValue = await eventTest.testGetValue(pingName);
   const counterTestValue = await counterTest.testGetValue(pingName);
   const stringTestValue = await stringTest.testGetValue(pingName);
-  // const fromLangValue = await fromLang.testGetValue(pingName);
-  // const toLangValue = await toLang.testGetValue(pingName);
   const mostRecentEventTestValue = eventTestValue.slice().pop();
 
   console.log({
@@ -22,30 +22,37 @@ const submitAndAssert = async () => {
     mostRecentEventTestValue,
     counterTestValue,
     stringTestValue,
-    // fromLangValue,
-    // toLangValue,
   });
 
-  // assert.equal(eventTestValue.length, 1);
+  assert.equal(eventTestValue.length, 1);
   assert.equal(mostRecentEventTestValue.name, "event_test");
-  // assert.equal(counterTestValue, 1);
+  assert.equal(counterTestValue, 1);
   assert.equal(stringTestValue, testStrVal);
 
   telemetry.submit();
   assert.isUndefined(await eventTest.testGetValue(pingName));
   assert.isUndefined(await counterTest.testGetValue(pingName));
   assert.isUndefined(await stringTest.testGetValue(pingName));
-  // assert.isUndefined(await fromLang.testGetValue(pingName));
-  // assert.isUndefined(await toLang.testGetValue(pingName));
 };
 
 describe("Telemetry", function() {
   beforeEach(async function() {
-    // TODO: Reset Glean before running each test
-    // await Glean.testResetGlean("org-mozilla-bergamot-in-browser-tests");
+    // Reset Glean before running each test
+    await Glean.testResetGlean(config.telemetryAppId);
   });
 
-  it("test metrics collected via Glean.js directly", async function() {
+  it("test metrics collected", async function() {
+    console.info(this.test.fullTitle());
+
+    eventTest.record();
+    // eventTest.record({ from_lang: "from", to_lang: "to" })
+    counterTest.add();
+    stringTest.set(testStrVal);
+
+    await submitAndAssert();
+  });
+
+  it("test metrics collected again", async function() {
     console.info(this.test.fullTitle());
 
     eventTest.record();
