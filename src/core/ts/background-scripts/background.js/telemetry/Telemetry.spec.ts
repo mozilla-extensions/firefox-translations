53d6c5ca555fdf9272c05440c59ea3821433ea6e
Motin <motin@motin.eu>
2021-03-29 08:34:47 +0300

    Add more detailed test cases and info to telemetry tests to verify sync/async behaviors

--- src/core/ts/background-scripts/background.js/telemetry/Telemetry.spec.ts
+++ src/core/ts/background-scripts/background.js/telemetry/Telemetry.spec.ts
@@ -12,6 +12,7 @@ import {
   translationTime,
   wordsPerSecond,
 } from "./generated/performance";
+import { custom } from "./generated/pings";
 
 const testStrVal = "test";
 const pingName = "custom";
@@ -46,6 +47,12 @@ const submitAndAssert = async () => {
   // assert.isUndefined(await toLang.testGetValue(pingName));
 };
 
+const sleep = ms => {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+};
+
 describe("Telemetry", function() {
   beforeEach(async function() {
     // TODO: Reset Glean before running each test
@@ -76,14 +83,34 @@ describe("Telemetry", function() {
     await submitAndAssert();
   });
 
-  it("expect multiple simultaneous async metrics collection not to work when using Glean.js directly", async function() {
+  it("expect multiple simultaneous async metrics collection not to work when using Glean.js directly in an async function", async function() {
     console.info(this.test.fullTitle());
 
     const expected = {};
     const actual = {};
 
+    const asyncFunction = async function onTranslationAttemptConcluded(
+      from: string,
+      to: string,
+      $modelLoadTime: number,
+      $translationTime: number,
+      $wordsPerSecond: number,
+      beforeSubmit: () => Promise<void> = undefined,
+    ) {
+      fromLang.set(from);
+      toLang.set(to);
+      modelLoadTime.set(String($modelLoadTime));
+      translationTime.set(String($translationTime));
+      wordsPerSecond.set(String($wordsPerSecond));
+      if (beforeSubmit) {
+        await beforeSubmit();
+      }
+      custom.submit();
+    };
+
     // Dispatch 10 metrics collections simultaneously
-    await Promise.all(
+    const count = 10;
+    const promise = Promise.all(
       [...Array(10).keys()].map(index => {
         return (async () => {
           expected[index] = {
@@ -93,7 +120,12 @@ describe("Telemetry", function() {
             $translationTime: `${index}`,
             $wordsPerSecond: `${index}`,
           };
-          await telemetry.onTranslationAttemptConcluded(
+          console.log(
+            `Sleeping 1s before dispatching async function - ${index}`,
+          );
+          await sleep(1000);
+          console.log(`Dispatching async function - ${index}`);
+          await asyncFunction(
             `from${index}`,
             `to${index}`,
             index,
@@ -109,12 +141,130 @@ describe("Telemetry", function() {
               };
             },
           );
+          console.log(`Completed async function - ${index}`);
         })();
       }),
     );
+    console.log(`Dispatched ${count} async functions`);
+    await promise;
+    console.log(`${count} async functions finished executing asynchronously`);
 
     console.log({ actual, expected });
     assert.notDeepEqual(actual, expected);
+  });
+
+  it("expect multiple simultaneous async metrics collection to work when using Glean.js directly in a sync function", async function() {
+    console.info(this.test.fullTitle());
+
+    const expected = {};
+
+    const syncFunction = function onTranslationAttemptConcluded(
+      from: string,
+      to: string,
+      $modelLoadTime: number,
+      $translationTime: number,
+      $wordsPerSecond: number,
+    ) {
+      fromLang.set(from);
+      toLang.set(to);
+      modelLoadTime.set(String($modelLoadTime));
+      translationTime.set(String($translationTime));
+      wordsPerSecond.set(String($wordsPerSecond));
+      custom.submit();
+    };
+
+    // Dispatch 10 metrics collections simultaneously
+    const count = 10;
+    const promise = Promise.all(
+      [...Array(count).keys()].map(index => {
+        return (async () => {
+          expected[index] = {
+            from: `from${index}`,
+            to: `to${index}`,
+            $modelLoadTime: `${index}`,
+            $translationTime: `${index}`,
+            $wordsPerSecond: `${index}`,
+          };
+          console.log(
+            `Sleeping 1s before dispatching sync function - ${index}`,
+          );
+          await sleep(1000);
+          console.log(`Dispatching sync function - ${index}`);
+          syncFunction(`from${index}`, `to${index}`, index, index, index);
+          console.log(`Completed sync function - ${index}`);
+        })();
+      }),
+    );
+    console.log(`Dispatched ${count} sync functions`);
+    await promise;
+    console.log(`${count} sync functions finished executing asynchronously`);
+
+    console.log({ expected });
+    // assert.notDeepEqual(actual, expected);
+  });
+
+  it("expect multiple simultaneous async metrics collection to work when using Glean.js directly in a sync function in an async wrapper", async function() {
+    console.info(this.test.fullTitle());
+
+    const expected = {};
+
+    const syncFunction = function onTranslationAttemptConcluded(
+      from: string,
+      to: string,
+      $modelLoadTime: number,
+      $translationTime: number,
+      $wordsPerSecond: number,
+    ) {
+      fromLang.set(from);
+      toLang.set(to);
+      modelLoadTime.set(String($modelLoadTime));
+      translationTime.set(String($translationTime));
+      wordsPerSecond.set(String($wordsPerSecond));
+      custom.submit();
+    };
+
+    // Dispatch 10 metrics collections simultaneously
+    const count = 10;
+    const promise = Promise.all(
+      [...Array(count).keys()].map(index => {
+        return (async () => {
+          expected[index] = {
+            from: `from${index}`,
+            to: `to${index}`,
+            $modelLoadTime: `${index}`,
+            $translationTime: `${index}`,
+            $wordsPerSecond: `${index}`,
+          };
+          console.log(
+            `Sleeping 1s before dispatching sync function in an async wrapper - ${index}`,
+          );
+          await sleep(1000);
+          console.log(
+            `Dispatching sync function in an async wrapper - ${index}`,
+          );
+          /*
+          new Promise<void>((resolve) => {
+            syncFunction(`from${index}`, `to${index}`, index, index, index);
+            resolve();
+          })
+           */
+          await (async () => {
+            // await sleep(1000);
+            syncFunction(`from${index}`, `to${index}`, index, index, index);
+            // await sleep(1000);
+          })();
+          console.log(`Completed sync function in an async wrapper - ${index}`);
+        })();
+      }),
+    );
+    console.log(`Dispatched ${count} sync functions in async wrappers`);
+    await promise;
+    console.log(
+      `${count} sync functions in async wrappers finished executing asynchronously`,
+    );
+
+    console.log({ expected });
+    // assert.notDeepEqual(actual, expected);
   });
 
   it("expect multiple simultaneous async metrics collection to work when using Glean.js via GleanRecorder", async function() {
@@ -124,7 +274,8 @@ describe("Telemetry", function() {
     const actual = {};
 
     // Dispatch 10 metrics collections simultaneously
-    await Promise.all(
+    const count = 10;
+    const promise = Promise.all(
       [...Array(10).keys()].map(index => {
         return (async () => {
           expected[index] = {
@@ -135,7 +286,14 @@ describe("Telemetry", function() {
             $wordsPerSecond: `${index}`,
           };
 
-          return gleanRecorder.transaction(async () => {
+          console.log(
+            `Sleeping 1s before dispatching async function via GleanRecorder - ${index}`,
+          );
+          await sleep(1000);
+          console.log(
+            `Dispatching async function via GleanRecorder - ${index}`,
+          );
+          await gleanRecorder.transaction(async () => {
             await telemetry.onTranslationAttemptConcluded(
               `from${index}`,
               `to${index}`,
@@ -155,8 +313,14 @@ describe("Telemetry", function() {
               },
             );
           });
+          console.log(`Completed async function via GleanRecorder - ${index}`);
         })();
       }),
+    );
+    console.log(`Dispatched ${count} async functions via GleanRecorder`);
+    await promise;
+    console.log(
+      `${count} async functions via GleanRecorder finished executing asynchronously`,
     );
 
     console.log({ actual, expected });
