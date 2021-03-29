81e8884ee9ec1f7b660499d68bdff45c47f24c0d
Motin <motin@motin.eu>
2021-03-29 15:46:28 +0300

    Increase timeout waiting for test servers

--- test/e2e/tests/firefox-infobar-ui.spec.ts
+++ test/e2e/tests/firefox-infobar-ui.spec.ts
@@ -61,7 +61,7 @@ if (process.env.UI === "firefox-infobar-ui") {
     launchTestProxyServer();
     await waitOn({
       resources: ["tcp:localhost:4001", "tcp:localhost:8080"],
-      timeout: 1000, // timeout in ms, default Infinity
+      timeout: 2000, // timeout in ms, default Infinity
     });
   });
 
