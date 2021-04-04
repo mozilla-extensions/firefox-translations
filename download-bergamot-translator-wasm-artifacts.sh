1d9a57680c91abb28278a2183780aeaa4e3de2b5
Jerin Philip <jphilip@ed.ac.uk>
2021-04-04 12:31:16 +0000

    Update download-artifact script with CI artifact from recent commit

--- download-bergamot-translator-wasm-artifacts.sh
+++ download-bergamot-translator-wasm-artifacts.sh
@@ -1,9 +1,14 @@
 # The artifacts are built from this commit: https://github.com/mozilla/bergamot-translator/commit/bf28edad82660db6a9524ae0e7f05dab007f78ce
 # Links are fetched manually from https://app.circleci.com/pipelines/github/mozilla/bergamot-translator/14/workflows/395cfa6a-eb2d-439b-8b89-d5c4c18f1dba/jobs/15/artifacts
+
+# Edits:
+# https://app.circleci.com/pipelines/github/mozilla-extensions/bergamot-browser-extension/145/workflows/0c4b8045-444f-4265-aa31-2f240f9a6ceb/jobs/144
+
+CIRCLECI_BASE_URL="https://33-346428477-gh.circle-artifacts.com/0/build-wasm/wasm"
 if [ ! -d downloaded-bergamot-translator-wasm-artifacts ]; then
   mkdir downloaded-bergamot-translator-wasm-artifacts
   cd downloaded-bergamot-translator-wasm-artifacts
-  wget "https://15-346428477-gh.circle-artifacts.com/0/build-wasm/wasm/bergamot-translator-worker.data"
-  wget "https://15-346428477-gh.circle-artifacts.com/0/build-wasm/wasm/bergamot-translator-worker.js"
-  wget "https://15-346428477-gh.circle-artifacts.com/0/build-wasm/wasm/bergamot-translator-worker.wasm"
+  wget "${CIRCLECI_BASE_URL}/bergamot-translator-worker.data"
+  wget "${CIRCLECI_BASE_URL}/bergamot-translator-worker.js"
+  wget "${CIRCLECI_BASE_URL}/bergamot-translator-worker.wasm"
 fi
