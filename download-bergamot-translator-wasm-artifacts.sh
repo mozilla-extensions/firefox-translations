b09ac53b6311660575c16850adabef044d73debe
Jerin Philip <jphilip@ed.ac.uk>
2021-04-04 12:48:42 +0000

    Fix for URL (copy-paste error)

--- download-bergamot-translator-wasm-artifacts.sh
+++ download-bergamot-translator-wasm-artifacts.sh
@@ -2,7 +2,7 @@
 # Links are fetched manually from https://app.circleci.com/pipelines/github/mozilla/bergamot-translator/14/workflows/395cfa6a-eb2d-439b-8b89-d5c4c18f1dba/jobs/15/artifacts
 
 # Edits:
-# https://app.circleci.com/pipelines/github/mozilla-extensions/bergamot-browser-extension/145/workflows/0c4b8045-444f-4265-aa31-2f240f9a6ceb/jobs/144
+# https://app.circleci.com/pipelines/github/mozilla/bergamot-translator/32/workflows/77853ed8-8be7-4cd6-9014-1beaff836745/jobs/33/artifacts 
 
 CIRCLECI_BASE_URL="https://33-346428477-gh.circle-artifacts.com/0/build-wasm/wasm"
 if [ ! -d downloaded-bergamot-translator-wasm-artifacts ]; then
