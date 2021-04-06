764fe639859aac22c330ab14b449e777d9860368
Motin <motin@motin.eu>
2021-04-06 15:35:22 +0300

    Revert "Use platform-specific profiles - necessary for mitmproxy certificates to be recognized in ci"
    This reverts commit ddc4f6adb6c87887bd7bcd4aced9444aeac9a8a0.

--- test/e2e/utils/setupWebdriver.ts
+++ test/e2e/utils/setupWebdriver.ts
@@ -52,14 +52,8 @@ export const launchFirefox = async (): Promise<WebDriver> => {
   const options = new Options();
 
   // Route traffic through test-controlled proxy to be able to intercept telemetry requests
-  // (Uses a platform-specific profile that includes the mitmproxy-ca-cert.pem certificate)
-  const profilePath = join(
-    process.cwd(),
-    "test",
-    "e2e",
-    "profile",
-    process.platform,
-  );
+  // (Uses a profile that includes the mitmproxy-ca-cert.pem certificate)
+  const profilePath = join(process.cwd(), "test", "e2e", "profile");
   options.setProfile(profilePath);
   options.setProxy(
     proxy.manual({ http: "localhost:8080", https: "localhost:8080" }),
