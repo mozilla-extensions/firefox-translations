e6b703327a9dcd9476df3363b2cd0d3f970febf3
Motin <motin@motin.eu>
2021-04-14 09:51:04 +0300

    Updated links to bergamot-translator artifacts (restores CI for 1-2 days more + lifetime of circle ci cache) - workaround for https://github.com/mozilla/bergamot-translator/issues/12#issuecomment-819253430

--- download-bergamot-translator-wasm-artifacts.sh
+++ download-bergamot-translator-wasm-artifacts.sh
@@ -1,9 +1,9 @@
 # The artifacts are built from this commit: https://github.com/mozilla/bergamot-translator/commit/bf28edad82660db6a9524ae0e7f05dab007f78ce
-# Links are fetched manually from https://app.circleci.com/pipelines/github/mozilla/bergamot-translator/14/workflows/395cfa6a-eb2d-439b-8b89-d5c4c18f1dba/jobs/15/artifacts
+# Links were fetched manually from https://app.circleci.com/pipelines/github/mozilla/bergamot-translator/29/workflows/ee468970-db75-49ba-8b89-83eec2cef9cc/jobs/30/steps
 if [ ! -d downloaded-bergamot-translator-wasm-artifacts ]; then
   mkdir downloaded-bergamot-translator-wasm-artifacts
   cd downloaded-bergamot-translator-wasm-artifacts
-  wget "https://15-346428477-gh.circle-artifacts.com/0/build-wasm/wasm/bergamot-translator-worker.data"
-  wget "https://15-346428477-gh.circle-artifacts.com/0/build-wasm/wasm/bergamot-translator-worker.js"
-  wget "https://15-346428477-gh.circle-artifacts.com/0/build-wasm/wasm/bergamot-translator-worker.wasm"
+  wget "https://30-346428477-gh.circle-artifacts.com/0/build-wasm/wasm/bergamot-translator-worker.data"
+  wget "https://30-346428477-gh.circle-artifacts.com/0/build-wasm/wasm/bergamot-translator-worker.js"
+  wget "https://30-346428477-gh.circle-artifacts.com/0/build-wasm/wasm/bergamot-translator-worker.wasm"
 fi
