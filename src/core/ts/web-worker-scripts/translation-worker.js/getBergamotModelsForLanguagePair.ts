4d6d9ed1f60e4cfdaf605c1cc661e0a93f84af43
Motin <motin@motin.eu>
2021-04-26 12:26:55 +0300

    Replace cache.add with fetch+cache.put combo so that we have more control over what and when to cache downloaded model files

--- src/core/ts/web-worker-scripts/translation-worker.js/getBergamotModelsForLanguagePair.ts
+++ src/core/ts/web-worker-scripts/translation-worker.js/getBergamotModelsForLanguagePair.ts
@@ -27,21 +27,35 @@ export const getBergamotModelsForLanguagePair = async (
     modelFiles.map(async ({ url, name }) => {
       let response = await cache.match(url);
       if (!response) {
-        log(`Downloading model file from ${url}`);
-        try {
-          await cache.add(url);
-        } catch (err) {
-          if (err.name === "QuotaExceededError") {
-            // Don't bail just because we can't persist the model file across browser restarts
-            console.warn(err);
-            log("Ran into and ignored a QuotaExceededError");
-          } else {
-            throw err;
+        log(`Downloading model file ${name} from ${url}`);
+        const downloadResponse = await fetch(url);
+
+        log(`Response for ${url} from network is: ${downloadResponse.status}`);
+
+        // This avoids caching responses that we know are errors (i.e. HTTP status code of 4xx or 5xx).
+        if (downloadResponse.status < 400) {
+          // We call .clone() on the request since we might use it in a call to cache.put() later on.
+          // Both fetch() and cache.put() "consume" the request, so we need to make a copy.
+          // (see https://developer.mozilla.org/en-US/docs/Web/API/Request/clone)
+          try {
+            await cache.put(url, downloadResponse.clone());
+            response = await cache.match(url);
+          } catch (err) {
+            if (err.name === "QuotaExceededError") {
+              // Don't bail just because we can't persist the model file across browser restarts
+              console.warn(err);
+              log(`${name}: Ran into and ignored a QuotaExceededError`);
+              response = downloadResponse;
+            } else {
+              throw err;
+            }
           }
+        } else {
+          log(`${name}: Not caching the response to ${url}`);
+          response = downloadResponse;
         }
-        response = await cache.match(url);
       } else {
-        log(`Model file from ${url} previously downloaded already`);
+        log(`${name}: Model file from ${url} previously downloaded already`);
       }
       const blob = await response.blob();
       return { name, data: blob };
