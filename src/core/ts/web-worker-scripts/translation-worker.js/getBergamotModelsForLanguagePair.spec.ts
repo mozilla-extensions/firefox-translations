e8d69dd6f6eed532b051bdec84b46efdfb7eaaa3
Motin <motin@motin.eu>
2021-04-26 10:45:59 +0300

    Add simple in-browser test for model downloading

---
+++ src/core/ts/web-worker-scripts/translation-worker.js/getBergamotModelsForLanguagePair.spec.ts
@@ -0,0 +1,28 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { assert } from "chai";
+import { getBergamotModelsForLanguagePair } from "./getBergamotModelsForLanguagePair";
+
+// Using cache-polyfill to work around https://bugzilla.mozilla.org/show_bug.cgi?id=1575625
+import { caches } from "cache-polyfill";
+import { config } from "../../config";
+
+const log = console.info;
+
+describe("getBergamotModelsForLanguagePair", function() {
+  it("esen", async function() {
+    const languagePair = "esen";
+
+    const cache = await caches.open("tests:bergamot-models");
+    const blobs = await getBergamotModelsForLanguagePair(
+      languagePair,
+      config.bergamotModelsBaseUrl,
+      cache,
+      log,
+    );
+
+    assert.equal(blobs.length, 3);
+  });
+});
