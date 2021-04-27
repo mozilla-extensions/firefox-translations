c95abb9b56e26fbb848633c862ff9c2d65fc84d4
Motin <motin@motin.eu>
2021-04-27 14:26:09 +0300

    Exclude 'translation-worker' webpack target from split chunks optimization since it leads to a broken web worker bundle (https://github.com/webpack/webpack/issues/7879)

--- webpack.config.js
+++ webpack.config.js
@@ -177,7 +177,16 @@ module.exports = {
       cacheGroups: {
         commons: {
           name: "commons",
-          chunks: "initial",
+          chunks(chunk) {
+            // exclude 'translation-worker' from split chunks since chunks
+            // are not loaded in web workers (https://github.com/webpack/webpack/issues/7879)
+            if (chunk.name === "translation-worker") {
+              return false;
+            }
+            // simulate the "initial" configuration value as per
+            // https://github.com/webpack/webpack/blob/4837c3ddb9da8e676c73d97460e19689dd9d4691/lib/optimize/SplitChunksPlugin.js#L248
+            return chunk.canBeInitial();
+          },
           minChunks: 2,
         },
       },
