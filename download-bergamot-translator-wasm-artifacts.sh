5bc1797a9fbc90ab3b4c68da3596d3f36abe2af3
Motin <motin@motin.eu>
2021-05-04 22:20:13 +0300

    Updated bergamot-translator to the latest one from https://github.com/mozilla/bergamot-translator/pull/51

--- download-bergamot-translator-wasm-artifacts.sh
+++ download-bergamot-translator-wasm-artifacts.sh
@@ -1,8 +1,8 @@
-# The artifacts are built from the same source code as this commit: https://github.com/mozilla/bergamot-translator/commit/8b2f660169512e9040aa32aa0dd6524b8e358a75
-# Links were fetched manually from https://app.circleci.com/pipelines/github/mozilla/bergamot-translator/63/workflows/4824fb5f-8947-4e9b-b49b-738a4d8341c6/jobs/68/artifacts
+# The artifacts are built from the same source code as this commit: https://github.com/mozilla/bergamot-translator/commit/a8cd998720bc34a6ab0d7615557cb402d4326a48
+# Links were fetched manually from https://app.circleci.com/pipelines/github/mozilla/bergamot-translator/64/workflows/6b9c3f45-f900-414d-8060-10cf2e6ec32b/jobs/69/artifacts
 if [ ! -d downloaded-bergamot-translator-wasm-artifacts ]; then
   mkdir downloaded-bergamot-translator-wasm-artifacts
   cd downloaded-bergamot-translator-wasm-artifacts
-  wget "https://68-346428477-gh.circle-artifacts.com/0/build-wasm/bergamot-translator-worker.js"
-  wget "https://68-346428477-gh.circle-artifacts.com/0/build-wasm/bergamot-translator-worker.wasm"
+  wget "https://69-346428477-gh.circle-artifacts.com/0/build-wasm/bergamot-translator-worker.js"
+  wget "https://69-346428477-gh.circle-artifacts.com/0/build-wasm/bergamot-translator-worker.wasm"
 fi
