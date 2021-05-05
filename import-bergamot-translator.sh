962d88d1d701e149736377661ba9e13af4a2639d
Motin <motin@motin.eu>
2021-05-05 13:36:41 +0300

    Make prettier available in circle ci at bergamot translator import step and re-enable auto-formatting at import state

--- import-bergamot-translator.sh
+++ import-bergamot-translator.sh
@@ -16,6 +16,7 @@ fi
 
 echo "* Constructing a TypeScript module from the emscripten JS glue code"
 TS_FILE=src/core/ts/web-worker-scripts/translation-worker.js/bergamot-translator-worker.ts
+yarn prettier "$ARTIFACTS_DIRECTORY/bergamot-translator-worker.js" --write
 echo "// @ts-nocheck" > $TS_FILE
 echo "" >> $TS_FILE
 echo "// Note: The source code in this file is imported from bergamot-translator via" >> $TS_FILE
@@ -24,6 +25,9 @@ echo "// Changes will be overwritten on each import!" >> $TS_FILE
 cat "$ARTIFACTS_DIRECTORY/bergamot-translator-worker.js" | sed 's/wasmBinaryFile = "/wasmBinaryFile = "wasm\//g' >> $TS_FILE
 echo "function addOnPreMain(cb) {__ATMAIN__.unshift(cb);}" >> $TS_FILE
 echo "export { addOnPreMain, Module, FS, WORKERFS };" >> $TS_FILE
+
+echo "* Autoformatting imported TypeScript module"
+yarn prettier src/core/ts/web-worker-scripts/translation-worker.js/bergamot-translator-worker.ts --write
 
 echo "* Copying bergamot-translator wasm artifact (as is)"
 cp "$ARTIFACTS_DIRECTORY/bergamot-translator-worker.wasm" src/core/static/wasm/bergamot-translator-worker.wasm
