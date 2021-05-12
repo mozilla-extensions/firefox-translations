347b37b57539a22ae7019fff26e80c1c83de91f2
Motin <motin@motin.eu>
2021-05-12 16:50:57 +0300

    Use official bergamot-translator v0.3.0 artifacts

--- download-bergamot-translator-wasm-artifacts.sh
+++ download-bergamot-translator-wasm-artifacts.sh
@@ -1,8 +1,8 @@
-# The artifacts are built from the same source code as this commit: https://github.com/mozilla/bergamot-translator/commit/fff70d6564964bfea081e747850f339df32e94f2
-# Links were fetched manually from https://app.circleci.com/pipelines/github/mozilla/bergamot-translator/74/workflows/439bdc32-fd8a-466b-80f6-259d53185f12/jobs/77/artifacts
+# Using artifacts from https://github.com/mozilla/bergamot-translator/releases/tag/v0.3.0
 if [ ! -d downloaded-bergamot-translator-wasm-artifacts ]; then
   mkdir downloaded-bergamot-translator-wasm-artifacts
   cd downloaded-bergamot-translator-wasm-artifacts
-  wget "https://77-346428477-gh.circle-artifacts.com/0/build-wasm/bergamot-translator-worker.js"
-  wget "https://77-346428477-gh.circle-artifacts.com/0/build-wasm/bergamot-translator-worker.wasm"
+  wget "https://github.com/mozilla/bergamot-translator/releases/download/v0.3.0/bergamot-translator-worker.js"
+  wget "https://github.com/mozilla/bergamot-translator/releases/download/v0.3.0/bergamot-translator-worker.wasm.gz"
+  gunzip bergamot-translator-worker.wasm.gz
 fi
