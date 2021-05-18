7ed6ddf3c56f840039a758675a8505aabdef0f58
Motin <motin@motin.eu>
2021-05-18 15:44:42 +0300

    Add bergamot-translator version info to extension log output (includes update of bergamot-translator from v0.3.0 to v0.3.1)

--- src/firefox-infobar-ui/ts/background-scripts/background.js/index.ts
+++ src/firefox-infobar-ui/ts/background-scripts/background.js/index.ts
@@ -18,6 +18,7 @@ import { MobxKeystoneBackgroundContextHost } from "../../../../core/ts/backgroun
 import { NativeTranslateUiBroker } from "./NativeTranslateUiBroker";
 import { contentScriptBergamotApiClientPortListener } from "../../../../core/ts/background-scripts/background.js/contentScriptBergamotApiClientPortListener";
 import { telemetry } from "../../../../core/ts/background-scripts/background.js/telemetry/Telemetry";
+import { BERGAMOT_VERSION_FULL } from "../../../../core/ts/web-worker-scripts/translation-worker.js/bergamot-translator-worker";
 const store = new Store(localStorageWrapper);
 /* eslint-disable no-unused-vars */
 // TODO: update typescript-eslint when support for this kind of declaration is supported
@@ -42,7 +43,7 @@ class ExtensionGlue {
     // Add version to extension log output to facilitate general troubleshooting
     const manifest = crossBrowser.runtime.getManifest();
     console.info(
-      `Extension ${manifest.name} version ${manifest.version} initializing.`,
+      `Extension ${manifest.name} version ${manifest.version} (with bergamot-translator ${BERGAMOT_VERSION_FULL}) initializing.`,
     );
 
     // Initiate the root extension state store
