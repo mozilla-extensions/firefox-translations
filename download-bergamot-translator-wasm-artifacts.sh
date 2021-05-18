ff42520e05114d678859b2c964458836b84cf278
Motin <motin@motin.eu>
2021-05-18 17:43:33 +0300

    Add bergamot-translator version info to extension log output (includes update of bergamot-translator from v0.3.0 to v0.3.1) (#136)

--- download-bergamot-translator-wasm-artifacts.sh
+++ download-bergamot-translator-wasm-artifacts.sh
@@ -2,7 +2,7 @@
 if [ ! -d downloaded-bergamot-translator-wasm-artifacts ]; then
   mkdir downloaded-bergamot-translator-wasm-artifacts
   cd downloaded-bergamot-translator-wasm-artifacts
-  wget "https://github.com/mozilla/bergamot-translator/releases/download/v0.3.0/bergamot-translator-worker.js"
-  wget "https://github.com/mozilla/bergamot-translator/releases/download/v0.3.0/bergamot-translator-worker.wasm.gz"
+  wget "https://github.com/mozilla/bergamot-translator/releases/download/v0.3.1/bergamot-translator-worker.js"
+  wget "https://github.com/mozilla/bergamot-translator/releases/download/v0.3.1/bergamot-translator-worker.wasm.gz"
   gunzip bergamot-translator-worker.wasm.gz
 fi
