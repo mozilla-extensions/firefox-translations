a797de89d3d7970b88943abcbc12c5f8111d32ad
Evgeny Pavlov <epavlov@mozilla.com>
2021-06-17 16:44:26 -0700

    Update models (#251)
    * Update bergamot-translator to the latest version from mozilla repo + Make sure CI status is set as failing when e2e tests fail (#59)
    
    * Update models
    
    * Update models links to 0.2.2
    
    Co-authored-by: Andre Natal <andrenatal@users.noreply.github.com>

--- import-bergamot-models.sh
+++ import-bergamot-models.sh
@@ -7,7 +7,7 @@ set -e
 
 echo "* Checkout out the relevant revision of the bergamot-models repo"
 MODELS_UPDATED=0
-MODELS_GIT_REV="3e8993bdf05b7294a99747675954e9166304592f" # v0.2.1
+MODELS_GIT_REV="0c8317c49f8a5484941ce0fc42e5ffaa79633b89" # v0.2.2
 if [ ! -d "bergamot-models" ]; then
   git clone --branch main --single-branch https://github.com/mozilla-applied-ml/bergamot-models
   MODELS_UPDATED=1
@@ -24,7 +24,7 @@ if [ "$MODELS_UPDATED" == "1" ]; then
   mkdir -p test/locally-hosted-files/models
   mv test/locally-hosted-files/models/dummy tmp
   rm -rf test/locally-hosted-files/models/*
-  cp -rf bergamot-models/prod/* test/locally-hosted-files/models
+  cp -rf bergamot-models/*/* test/locally-hosted-files/models
   ls -l test/locally-hosted-files/models/*
   mv tmp test/locally-hosted-files/models/dummy
 fi
