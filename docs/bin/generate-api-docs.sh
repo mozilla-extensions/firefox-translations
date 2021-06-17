d69e3b23f1425a1ecab81eb4a2da7ae76c842481
Motin <motin@motin.eu>
2021-06-17 15:18:18 +0300

    Add link-checking capability

--- docs/bin/generate-api-docs.sh
+++ docs/bin/generate-api-docs.sh
@@ -16,6 +16,13 @@ typedoc --options typedoc.json --gitRevision "v$REPO_PACKAGE_VERSION"
 
 cd dev
 
+# Remove the first line (includes breadcrumbs) from the individual pages
+# since it is superfluous when results are rendered in mdBook and the
+# links in the breadcrumbs cause some linkchecking warnings
+for FILE in api-docs/**/*.md; do
+  tail -n +2 "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
+done
+
 # Create links to all the individual pages in a file that eventually
 # ends up in SUMMARY.md (an mdBook requirement is that all files
 # are linked in SUMMARY.md)
