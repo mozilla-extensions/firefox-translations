b6dc2637c62f9736f33300c7e6ba17409db37137
evgeny pavlov <epavlov@mozilla.com>
2021-06-17 12:44:20 -0700

    Update models links to 0.2.2

--- import-bergamot-models.sh
+++ import-bergamot-models.sh
@@ -7,7 +7,7 @@ set -e
 
 echo "* Checkout out the relevant revision of the bergamot-models repo"
 MODELS_UPDATED=0
-MODELS_GIT_REV="3e8993bdf05b7294a99747675954e9166304592f" # v0.2.1
+MODELS_GIT_REV="0c8317c49f8a5484941ce0fc42e5ffaa79633b89" # v0.2.2
 if [ ! -d "bergamot-models" ]; then
   git clone --branch main --single-branch https://github.com/mozilla-applied-ml/bergamot-models
   MODELS_UPDATED=1
