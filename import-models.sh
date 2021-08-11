0afbea3376136d7ddccb33b1f23c0851a89a5d2b
evgeny pavlov <epavlov@mozilla.com>
2021-08-11 16:48:42 -0700

    Update models

--- import-models.sh
+++ import-models.sh
@@ -7,7 +7,7 @@ set -e
 
 echo "* Checkout out the relevant revision of the firefox-translations-models repo"
 MODELS_UPDATED=0
-MODELS_GIT_REV="a8158d24c78fc9b8bea6b3e94c99ee370210bd84" # v0.2.5
+MODELS_GIT_REV="2473ea093215446a76e8ae3915b2bd947c5659eb" # v0.2.6
 if [ ! -d "firefox-translations-models" ]; then
   git clone --branch main --single-branch https://github.com/mozilla/firefox-translations-models
   MODELS_UPDATED=1
